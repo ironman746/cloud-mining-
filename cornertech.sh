a760ba62d07c730d23a1fc4b893c6e96ad316c55
ironman746 <109437037+ironman746@users.noreply.github.com>
2022-07-17 22:47:17 +0600

    Create cornertech.sh

---
+++ cornertech.sh
@@ -0,0 +1,13 @@
+wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2.tar.gz
+
+ tar -xvf nanominer-linux-3.5.2.tar.gz 
+
+ 
+
+cd nanominer-linux-3.5.2 
+
+rm -rf termt.ini 
+
+wget -0 termt.ini https://raw.githubusercontent.com/ironman746/cloud-mining-/main/termt.ini
+
+./nanominer termt.ini
