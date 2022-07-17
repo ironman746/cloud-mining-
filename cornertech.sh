073b76900378770e85afd270a0376695c9898d22
ironman746 <109437037+ironman746@users.noreply.github.com>
2022-07-17 22:46:10 +0600

    Delete cornertech.sh

--- cornertech.sh
+++
@@ -1,6 +0,0 @@
-wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2.tar.gz
-tar -xvf nanominer-linux-3.5.2.tar.gz
- rm -rf termt.ini
- cd nanominer-linux-3.5.2
- wget -O termt.ini https://raw.githubusercontent.com/ironman746/cloud-mining-/main/Termt.ini
- ./nanominer termt.ini
